5b71bd4aa8a5405bedbcbadbf17fe142dab00679
Fran Navarro <fjnavarrob@gmail.com>
2017-06-01 23:47:48 +0200

    refactoring + Aeropython

---
+++ scripts/mi_primer_script.py
@@ -0,0 +1,11 @@
+import math
+     
+print("¡Hola gente del curso de Python para el análisis de datos!")
+print("¿Cuántos somos hoy en el curso?")
+
+number = input()
+number = int(number)
+root = math.sqrt(number)
+
+print("Ufff! eso es un montón! espero que aprendáis mucho")
+print("Por cierto, la raiz de %i es %f" %(number, root))
